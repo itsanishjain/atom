d7aee031bf1ef204d34cb53783d4fe5e8f3aa1d7
anishjain <helloanishjain@gmail.com>
2022-12-18 15:31:46 +0530

    add Coinmarket cap api and PK

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,9 @@
+import axios from "axios";
 import React, { useState, useContext } from "react";
 import { Web3ModalContext } from "../src/context/Web3ModalProvider";
 import { contractAddress, contractABI } from "../src/utils/constants";
+
+var newWeb3 = require("web3");
 
 const Home = () => {
   const [number, setNumber] = useState(1);
@@ -25,11 +28,42 @@ const Home = () => {
     console.log(tx, "DONE");
   };
 
+  const generateMessage = () => {
+    return Math.floor(Math.random() * 1000000).toString();
+  };
+
+  const XDC_ACCOUNT_2_PK =
+    "f3f7097ebda3883ecc6cf8bfb166cd3fa3ba6f8a9a54cf1873539a94e2827e9f";
+
+  const singAMessage = async () => {
+    console.log(newWeb3);
+    var aweb3 = new newWeb3();
+
+    // const signedMessage = await aweb3.eth.accounts
+    //   .sign(generateMessage(), XDC_ACCOUNT_2_PK)
+    //   .catch((e) => console.log(e));
+
+    // let message = generateMessage();
+
+    let response = await axios.get("/api/coinmarket");
+
+    let XDC_PRICE_USD = response.data.result.data[2634].quote.USD.price;
+
+    // console.log(response.data.result.data[2634].quote.USD.price);
+
+    // console.log(data.data.quote.USD.price);
+
+    var signature = aweb3.eth.accounts.sign(XDC_PRICE_USD, XDC_ACCOUNT_2_PK);
+
+    console.log({ signature });
+  };
+
   return (
-    <div className="mt-8 w-full p-4 md:w-1/2 mx-auto">
+    <div className="mt-8 w-full p-4 md:w-1/2 mx-auto flex flex-col space-y-8">
+      <button onClick={singAMessage}>Sign A message</button>
       <button onClick={getBlockNumber}>GET NUMBER</button>
       <p>Number is {number}</p>
-      <form onSubmit={setBlockNumber}>
+      <form className="p-4 space-y-4" onSubmit={setBlockNumber}>
         <input
           type="number"
           name="number"
