b2ba242d1477ef3f9f64276748d90ac17c056ad1
anishjain <helloanishjain@gmail.com>
2022-12-20 00:02:27 +0530

    updated UI

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className="App">
       <Head>
-        <title>Atom</title>
+        <title>Atomizer</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
@@ -118,15 +118,15 @@ export default function Home() {
             </div>
 
             <div className="box-border order-first w-full text-black border-solid md:w-1/2 md:pl-10 md:order-none">
-              <h2 className="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl text-yellow-400">
+              <h2 className="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl text-indigo-400">
                 Build a direct, meaningful connection with your audience
               </h2>
-              <p className="pt-4 pb-8 m-0 leading-7 text-yellow-100 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg">
+              <p className="pt-4 pb-8 m-0 leading-7 text-indigo-100 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg">
                 No ads, no trolls, no algorithms.
               </p>
               <ul className="p-0 m-0 leading-6 border-0 border-gray-300 space-y-2">
                 <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid flex">
-                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-500 rounded-full">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-indigo-500 rounded-full">
                     <span className="text-sm font-bold">✓</span>
                   </span>
                   With W3Commerce, its not about likes and views, you can really{" "}
@@ -134,7 +134,7 @@ export default function Home() {
                   connect with your fans.
                 </li>
                 <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid flex">
-                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-500 rounded-full">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-indigo-500 rounded-full">
                     <span className="text-sm font-bold">✓</span>
                   </span>
                   ignore the haters and focus on the positive. Id rather have
@@ -142,7 +142,7 @@ export default function Home() {
                   patrons than 3 million Instagram followers
                 </li>
                 <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid flex">
-                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-500 rounded-full">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-indigo-500 rounded-full">
                     <span className="text-sm font-bold">✓</span>
                   </span>
                   Adding support for multiple wallets to the login process
@@ -165,16 +165,16 @@ export default function Home() {
             </div>
 
             <div className="box-border order-first w-full text-black border-solid md:w-1/2 md:pl-10 md:order-none">
-              <h2 className="m-0 text-xl font-semibold leading-tight border-0 border-yellow-300 lg:text-3xl md:text-2xl text-yellow-400">
+              <h2 className="m-0 text-xl font-semibold leading-tight border-0 border-indigo-300 lg:text-3xl md:text-2xl text-indigo-400">
                 Develop a recurring income stream Stop rolling the dice of ad
                 revenue and per-stream payouts.
               </h2>
-              <p className="pt-4 pb-8 m-0 leading-7 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg text-yellow-100">
+              <p className="pt-4 pb-8 m-0 leading-7 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg text-indigo-100">
                 Get recurring income through monthly payments from your patrons.
               </p>
               <ul className="p-0 m-0 leading-6 border-0 border-gray-300 space-y-2">
                 <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid flex">
-                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-500 rounded-full">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-indigo-500 rounded-full">
                     <span className="text-sm font-bold">✓</span>
                   </span>
                   Using Twitter and Discord to authorize
@@ -182,7 +182,7 @@ export default function Home() {
                   projects so that users can&apos;t spam their W3Commerce
                 </li>
                 <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid flex">
-                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-500 rounded-full">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-indigo-500 rounded-full">
                     <span className="text-sm font-bold">✓</span>
                   </span>
                   Filtering users with multiple accounts automatically
@@ -190,7 +190,7 @@ export default function Home() {
                   by keeping track of their transaction activities
                 </li>
                 <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid flex">
-                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-500 rounded-full">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-indigo-500 rounded-full">
                     <span className="text-sm font-bold">✓</span>
                   </span>
                   Adding support for multiple wallets to the login process
@@ -213,22 +213,22 @@ export default function Home() {
             </div>
 
             <div className="box-border order-first w-full text-black border-solid md:w-1/2 md:pl-10 md:order-none">
-              <h2 className="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl text-yellow-400">
+              <h2 className="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl text-indigo-400">
                 give back creative products
               </h2>
-              <p className="pt-4 pb-8 m-0 leading-7 text-yellow-100 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg">
+              <p className="pt-4 pb-8 m-0 leading-7 text-indigo-100 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg">
                 Create what you want and what your audience loves.
               </p>
               <ul className="p-0 m-0 leading-6 border-0 border-gray-300 space-y-2">
                 <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid flex">
-                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-500 rounded-full">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-indigo-500 rounded-full">
                     <span className="text-sm font-bold">✓</span>
                   </span>
                   No ads, no trolls, no algorithms. Enjoy direct access and
                   deeper
                 </li>
                 <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid flex">
-                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-500 rounded-full">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-indigo-500 rounded-full">
                     <span className="text-sm font-bold">✓</span>
                   </span>
                   You dont have to conform to popular taste or the constraints
@@ -236,7 +236,7 @@ export default function Home() {
                   ad-based monetisation models.
                 </li>
                 <li className="box-border relative py-1 pl-0 text-left text-gray-500 border-solid flex">
-                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-yellow-500 rounded-full">
+                  <span className="inline-flex items-center justify-center w-6 h-6 mr-2 text-white bg-indigo-500 rounded-full">
                     <span className="text-sm font-bold">✓</span>
                   </span>
                   Adding support for multiple wallets to the login process
--- src/components/Header.js
+++ src/components/Header.js
@@ -6,12 +6,11 @@ import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 import { Web3ModalContext } from "../context/Web3ModalProvider";
 import Link from "next/link";
+import Banner from "./Banner";
 
 const navigation = [
-  { name: "Product", href: "#" },
   { name: "Features", href: "#feature" },
-  { name: "Marketplace", href: "#" },
-  { name: "Company", href: "#" },
+  { name: "Main app", href: "/home" },
 ];
 
 export default function Header() {
@@ -32,33 +31,7 @@ export default function Header() {
 
   return (
     <div className="isolate bg-white">
-      {/* <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]">
-        <svg
-          className="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
-          viewBox="0 0 1155 678"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
-            fillOpacity=".3"
-            d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-          />
-          <defs>
-            <linearGradient
-              id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
-              x1="1155.49"
-              x2="-78.208"
-              y1=".177"
-              y2="474.645"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#9089FC" />
-              <stop offset={1} stopColor="#FF80B5" />
-            </linearGradient>
-          </defs>
-        </svg>
-      </div> */}
+      <Banner />
       <div className="px-6 pt-6 lg:px-8">
         <div>
           <nav
@@ -89,13 +62,11 @@ export default function Header() {
             </div>
             <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-center lg:gap-x-12">
               {navigation.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className="font-semibold text-gray-900 hover:text-gray-900"
-                >
-                  {item.name}
-                </a>
+                <Link href={item.href} key={item.name} passHref>
+                  <a className="font-semibold text-gray-900 hover:text-gray-900">
+                    {item.name}
+                  </a>
+                </Link>
               ))}
             </div>
             <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
